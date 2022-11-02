47204eb20af876ab319724d1c66524ffebbed7f6
JonasonWu <jw5911@nyu.edu>
2022-11-02 16:41:15 -0400

    Tried fixing it such that tests can run on local

--- tests/test_distance_converter.py
+++ tests/test_distance_converter.py
@@ -1,12 +1,9 @@
 import pytest
-from converterpackageteam10 import distance_converter
+from src.converterpackageteam10 import distance_converter
 
 class Tests:
 
-    def example_fixture(self):
-        yield
-
-    def test_sanity_check(self, example_fixture):
+    def test_sanity_check(self):
         expected = True # the value we expect to be present
         actual = True # the value we see in reality
         assert actual == expected, "Expected True to be equal to True!"
