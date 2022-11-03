d0b9d6daf12c2e6e137c44550bf3a3ab3a0022eb
JonasonWu <jw5911@nyu.edu>
2022-11-03 07:59:41 -0400

    Made alias additions for distance_converter. Added an additional test case to make sure functions can be called.

--- tests/test_distance_converter.py
+++ tests/test_distance_converter.py
@@ -7,3 +7,12 @@ class Tests:
         expected = True # the value we expect to be present
         actual = True # the value we see in reality
         assert actual == expected, "Expected True to be equal to True!"
+    
+    def test_convert_meter_to_kilometer(self):
+        actual = distance_converter.convert_distance("1000", "m", "km")
+        expected = "1.0"
+        assert actual == expected, "Expected conversion of 1000 meter to equal 1 kilometer"
+    def test_convert_meter_to_kilometer(self):
+        actual = distance_converter.convert_distance("1000", "meter", "kilometer")
+        expected = "1.0"
+        assert actual == expected, "Expected conversion of 1000 meter to equal 1 kilometer"
