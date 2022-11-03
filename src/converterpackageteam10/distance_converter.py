3386562c18a66ae0a7e1ed75095e44bc1fc8c489
JonasonWu <jw5911@nyu.edu>
2022-11-03 19:36:57 -0400

    Completed the test cases for distance_converter

--- src/converterpackageteam10/distance_converter.py
+++ src/converterpackageteam10/distance_converter.py
@@ -12,11 +12,11 @@ __alias_units = {
 __conversions = {
     "in": {"in": 1, "ft": 1/12, "mi": 1/63360, "mm": 25.4, "cm": 2.54, "m": 0.0254, "km": 2.54e-5}, 
     "ft": {"in": 12, "ft": 1, "mi": 1/5280, "mm": 304.8, "cm": 30.48, "m": 0.3048, "km": 3.048e-4},
-    "mi": {"in": 63360, "ft": 5280, "mi": 1, "mm": 1609340, "cm": 160934, "m": 1609.34, "km": 1.60934},
-    "mm": {"in": 1/25.4, "ft": 1/304.8, "mi": 1/1609340, "mm": 1, "cm": 1/10, "m": 1/1000, "km": 1e-6},
-    "cm": {"in": 1/2.54, "ft": 1/30.48, "mi": 1/160934, "mm": 10, "cm": 1, "m": 1/100, "km": 1e-5},
-    "m": {"in": 39.3701, "ft": 3.28084, "mi": 1/1609.34, "mm": 1000, "cm": 100, "m": 1, "km": 1e-3},
-    "km": {"in": 39370.1, "ft": 3280.84, "mi": 1/1.60934, "mm": 1e6, "cm": 1e5, "m": 1000, "km": 1}
+    "mi": {"in": 63360, "ft": 5280, "mi": 1, "mm": 1609344, "cm": 160934.4, "m": 1609.344, "km": 1.609344},
+    "mm": {"in": 1/25.4, "ft": 1/304.8, "mi": 1/1609344, "mm": 1, "cm": 1/10, "m": 1/1000, "km": 1e-6},
+    "cm": {"in": 1/2.54, "ft": 1/30.48, "mi": 1/160934.4, "mm": 10, "cm": 1, "m": 1/100, "km": 1e-5},
+    "m": {"in": 1/0.0254, "ft": 3.28084, "mi": 1/1609.344, "mm": 1000, "cm": 100, "m": 1, "km": 1e-3},
+    "km": {"in": 39370.1, "ft": 3280.84, "mi": 1/1.609344, "mm": 1e6, "cm": 1e5, "m": 1000, "km": 1}
 }
 
 def allowed_conversions():
@@ -24,12 +24,6 @@ def allowed_conversions():
 
 
 def convert_distance(num, unit1, unit2):
-    
-    if unit1 in __alias_units:
-        unit1 = __alias_units[unit1]
-    if unit2 in __alias_units:
-        unit2 = __alias_units[unit2]
-    
     if unit1 in __units:
         if unit2 in __units:
             return float(num) * __conversions[unit1][unit2]
@@ -40,4 +34,4 @@ def convert_distance(num, unit1, unit2):
 
 class UnitNotFound(Exception):
     def __init__(self, unit):
-        super().__init__("The unit " + unit + " is not listed as a valid convertible.")
+        super().__init__("The unit " + str(unit) + " is not listed as a valid convertible.")
--- tests/test_distance_converter.py
+++ tests/test_distance_converter.py
@@ -3,8 +3,8 @@ from src.converterpackageteam10 import distance_converter
 
 class TestDistanceConverter:
     #This is the tolerance for the conversions. Since conversions will result in 
-    #  floating points, the conversion may be slightly off than the actual.
-    tol = 0.00001
+    #  floating points, the conversion might be slightly off than the actual.
+    tol = 1e-6
 
     # Sanity check to make sure that tests can run
     def test_sanity_check(self):
@@ -21,6 +21,32 @@ class TestDistanceConverter:
         except AttributeError as e:
             assert True
     
+    def test_invalid_unit(self):
+        try:
+            distance_converter.convert_distance(10, None, None)
+            assert False, "Invalid unit parameters should result in UnitNotFound exception."
+        except distance_converter.UnitNotFound as e:
+            assert True
+    def test_invalid_unit_2(self):
+        try:
+            distance_converter.convert_distance(100, "non-existent", "in")
+            assert False, "Since unit1 paramter is invalid, UnitNotFound exception should be thrown"
+        except distance_converter.UnitNotFound as e:
+            assert True
+    def test_invalid_unit_3(self):
+        try:
+            distance_converter.convert_distance(100, "in", "non-existent")
+            assert False, "Since unit2 paramter is invalid, UnitNotFound exception should be thrown"
+        except distance_converter.UnitNotFound as e:
+            assert True
+    def test_invalid_num(self):
+        try:
+            distance_converter.convert_distance("abasd", "in", "in")
+            assert False, "Invalid number input should not be convertible. Exception should be thrown."
+        except Exception as e:
+            assert True
+
+
     def test_convert_inches_to_other(self):
         # Note: We assume that only the conversion of inches exist.
         unit1 = 'in'
@@ -89,8 +115,8 @@ class TestDistanceConverter:
         # Note: We have the addition of milimeter conversions.
         unit1 = 'cm'
         unit2List = ["in", "ft", "mi", "mm", "cm"]
-        numList = [2.54, 30.48, 160934.4, .1, 1]
-        resultList = [1, 1, 1, 1, 1]
+        numList = [2.54, 30.48, 160934.4, 1, 1]
+        resultList = [1, 1, 1, 10, 1]
         for num, unit2, res in zip(numList, unit2List, resultList):
             #First, test the conversion from unit1 -> u2
             expected = distance_converter.convert_distance(num, unit1, unit2)
@@ -101,4 +127,35 @@ class TestDistanceConverter:
             rev_actual = num
             assert abs(rev_actual-rev_expected) <= TestDistanceConverter.tol, "The conversion of " + unit2 + " to " + unit1 + " fails"
 
+    def test_convert_meter_to_other(self):
+        # Note: We have the addition of meter conversions.
+        unit1 = 'm'
+        unit2List = ["in", "ft", "mi", "mm", "cm", "m"]
+        numList = [.254, 3.048, 16093.44, .01, .1, 1]
+        resultList = [10, 10, 10, 10, 10, 1]
+        for num, unit2, res in zip(numList, unit2List, resultList):
+            #First, test the conversion from unit1 -> u2
+            expected = distance_converter.convert_distance(num, unit1, unit2)
+            actual = res
+            assert abs(actual-expected) <= TestDistanceConverter.tol, "The conversion of " + unit1 + " to " + unit2 + " fails"
+            #Then, make sure the conversion from u2 -> unit1 also works out.
+            rev_expected = distance_converter.convert_distance(res, unit2, unit1)
+            rev_actual = num
+            assert abs(rev_actual-rev_expected) <= TestDistanceConverter.tol, "The conversion of " + unit2 + " to " + unit1 + " fails"
+
+    def test_convert_kilometer_to_other(self):
+        # Note: We have the addition of kilometer conversions.
+        unit1 = 'km'
+        unit2List = ["in", "ft", "mi", "mm", "cm", "m", "km"]
+        numList = [1, 1, 1.609344, 1, 1, 1, 1]
+        resultList = [39370.1, 3280.84, 1, 1000000, 100000, 1000, 1]
+        for num, unit2, res in zip(numList, unit2List, resultList):
+            #First, test the conversion from unit1 -> u2
+            expected = distance_converter.convert_distance(num, unit1, unit2)
+            actual = res
+            assert abs(actual-expected) <= TestDistanceConverter.tol, "The conversion of " + unit1 + " to " + unit2 + " fails"
+            #Then, make sure the conversion from u2 -> unit1 also works out.
+            rev_expected = distance_converter.convert_distance(res, unit2, unit1)
+            rev_actual = num
+            assert abs(rev_actual-rev_expected) <= TestDistanceConverter.tol, "The conversion of " + unit2 + " to " + unit1 + " fails"
 
