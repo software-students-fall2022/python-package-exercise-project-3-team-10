89bbe2bb1f8e52e1d952acfced44ed2bd1b1faa0
JonasonWu <jw5911@nyu.edu>
2022-11-09 15:49:30 -0500

    Added test case for age_converter

--- tests/test_age_calc.py
+++ tests/test_age_calc.py
@@ -1,6 +1,7 @@
 import pytest
 from converterpackageteam10 import age_converter
 import re
+import datetime
 
 class Test_Age_Calc:
 
@@ -29,8 +30,36 @@ class Test_Age_Calc:
                 if result_format.match(result) is not None:
                     actual = False
                 else:
-                    actual = True
- 
-        
+                    actual = True        
         assert actual == True, f"Expected {result} to be a different format!"
     
+    def test_age_calc_valid_years(self):
+        result = age_converter.calc_age("Jan 1 2001 12:00AM", "years")
+        today = datetime.date.today()
+        expected = today.year - 2001
+        assert isinstance(result, str), f"Expected {result} to be a string!"
+    
+    def test_age_calc_valid_months(self):
+        result = age_converter.calc_age("Jan 1 2001 12:00AM", "years")
+        today = datetime.date.today()
+        expected = today.year - 2001
+        assert expected == int(result.split()[0]), f"Expected {result} to be a string!"
+    
+    def test_age_calc_no_exceptions(self):
+        years = [2001, 2010, 2021]
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+                'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        days = [1, 10, 12, 15, 18, 21, 24, 27]
+        units = ['actual', 'years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds']
+        try:
+            for year in years:
+                for month in months:
+                    for day in days:
+                        for unit in units:
+                            dob = " ".join([str(month), str(day), str(year), "12:00AM"])
+                            age_converter.calc_age(dob, unit)
+        except ValueError as e:
+            assert False, "ValueError: " + e.args[0]
+        except Exception as e:
+            assert False, "Exception: " + e.args[0]
+        assert True
